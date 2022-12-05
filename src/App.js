6f2ed89c388a66659208e72a4905d7594b582a28
Devansh0309 <samdeva0309@gmail.com>
2022-12-05 12:03:08 +0530

    Correctedsquareboxesusinggridpropertyincss

--- src/App.js
+++ src/App.js
@@ -41,14 +41,14 @@ useEffect(()=>{
   let horizontal=[]
   let vertical=[]
   let squares=[]
-  for(let i=0;i<row*col+row+col;i++){
+  for(let i=0;i<=row*col+row+col;i++){
     arr.push(i)
   }
   for(let i=0;i<row*col+col;i++){
     horizontal.push({key:i,type:'horizontal',isClicked:false})
   }
   for(let i=0;i<row*col+row;i++){
-    vertical.push({key:i,type:'vertical',isClicked:false})
+    vertical.push({key:i,type:'vertical',isClicked:true})
   }
   for(let i=0;i<row*col;i++){
     squares.push({allClicked:false})
@@ -207,12 +207,10 @@ const setClick=(id,type)=>{
     // setHorizontalButtons(...horizontalButtons,{key:id,type:'horizontal',isClicked:true})
   }
 }
-const GridStyle={
-  gridTemplateColumn:`repeat(${col},1fr)`,
-  // border:"1px solid red"
-}
+
   return (
     <div className="App">
+      {/* {console.log(sel)} */}
      <select onChange={(e)=>{setBox([]);
       setSelect(e.target.value);
       setNumberOfSquares(0)
@@ -222,50 +220,74 @@ const GridStyle={
       <option value="3*4">3 x 4</option>
       <option value="7*8">7 x 8</option>
      </select>
-
+    <br/> <br/>
   {numberOfSquares<row*col?
+  (<>
   <div className='gridBox' 
   style={{display:"grid",
-  gridTemplateColumns:`repeat(${col+1},1fr)`}}
+  gridTemplateColumns:`repeat(${col+1},1fr)`,
+gridTemplateRows:`repeat(${row+1},calc(600px/${col+1}))`}}
   >
   {
   Box.map((item)=>
-  item%(col+1) ===col?
-  <div className='onebox'>
-
-    <button className='sidelastbtn' key={item} onClick={()=>{setClick(item,'vertical');areAllClicked(item,'vertical',player)}}></button>
-    {/* <SideLastbtn key={item} clicked={setClick} areAllClicked={areAllClicked}/> */}
-
-  </div> :item>=row*(col+1)?
-    <div className='onebox'>
-
-      <button className='upperbtn' key={item-Math.floor(item/(col+1))} onClick={()=>{setClick(item-Math.floor(item/(col+1)),'horizontal');areAllClicked(item-Math.floor(item/(col+1)),'horizontal',player)}}></button>
-      {/* <Upperbtn key={item-Math.floor(item/(col+1))} clicked={setClick} areAllClicked={areAllClicked}/> */}
-
+  item%(col+1) ===col && item<row*col+row+col?
+    <div className='twobox' style={{display:'flex',flexDirection:'column'}}>
+      <div style={{backgroundColor:'black',width:'20%',height:'20%'}}>
+      </div>
+      <button className='sidelastbtn' key={item} onClick={()=>{setClick(item,'vertical');areAllClicked(item,'vertical',player)}}></button>
     </div>
-   :<div key={item} className='onebox'>
+  : 
+    item>=row*(col+1)?
+      item<row*col+row+col?
+        <div className='twobox'  style={{display:'flex'}}>
+          <div style={{backgroundColor:'black',width:'20%',height:'20%'}}>
+          </div>
+          <button className='lowerbtn' key={item-Math.floor(item/(col+1))} onClick={()=>{setClick(item-Math.floor(item/(col+1)),'horizontal');areAllClicked(item-Math.floor(item/(col+1)),'horizontal',player)}}></button>
+        </div>
+      :
+        <div style={{backgroundColor:'black',width:'20%',height:'20%'}}>
+        </div>
 
-      {/* <Upperbtn key={item-Math.floor(item/(col+1))} clicked={setClick}/> */}
-      <button className='upperbtn' key={item-Math.floor(item/(col+1))} onClick={()=>{setClick(item-Math.floor(item/(col+1)),'horizontal');areAllClicked(item-Math.floor(item/(col+1)),'horizontal',player)}}></button>
-
+    :
+      <div key={item} className='onebox'>
+      <div  style={{display:"flex",height:"20%"}}>
+        <div style={{backgroundColor:'black',width:'20%'}}>
+        </div>
+        <button className='upperbtn' key={item-Math.floor(item/(col+1))} onClick={()=>{setClick(item-Math.floor(item/(col+1)),'horizontal');areAllClicked(item-Math.floor(item/(col+1)),'horizontal',player)}}></button>
+      </div>
+      
       <div  style={{display:"flex",height:"80%"}}>
 
         <button className='sidebtn' key={item} onClick={()=>{setClick(item,'vertical');areAllClicked(item,'vertical',player)}}></button>
-        {/* <Sidebtn key={item} clicked={setClick} areAllClicked={areAllClicked}/> */}
+        
 
         <div className='innerBox' style={{backgroundColor:squaresColors[item-Math.floor(item/(col+1))].allClicked?'yellow':'lightgrey'}}>hi{item}</div>
-        {/* <InnerBox areAllClicked={squaresColors[item].allClicked} key={item}/> */}
+        
       </div>
-    </div>
-    )
-    }
-    </div>
-    :
-    sel!='Select size here'?
-    <div style={{margin:'auto'}}>
-      Player{player1Score>player2Score?'1':player1Score===player2Score?'s Tied and no one':'2'} won!
-      </div>:''
+      </div>
+  )
   }
+  </div>
+  <span style={{display:'inline-block'}}>hello</span>
+  </>
+  )
+  :
+  sel!=='Select size here'?
+    <div style={{margin:'auto'}}>
+      {/* {setSelect('Select size here')} */}
+      Player{player1Score>player2Score?'1':player1Score===player2Score?'s Tied and no one':'2'} won!
+    </div>
+  :''
+  }
+       {/* <Upperbtn key={item-Math.floor(item/(col+1))} clicked={setClick} areAllClicked={areAllClicked}/> */}
+      {/* <Upperbtn key={item-Math.floor(item/(col+1))} clicked={setClick}/> */}
+     {/* <Sidebtn key={item} clicked={setClick} areAllClicked={areAllClicked}/> */}
+    {/* <InnerBox areAllClicked={squaresColors[item].allClicked} key={item}/> */}
+     {/* <SideLastbtn key={item} clicked={setClick} areAllClicked={areAllClicked}/> */}
+     {/* const GridStyle={
+        gridTemplateColumn:`repeat(${col},1fr)`,
+       // border:"1px solid red"
+     } */}
      {/* Idea for rendering square color on click of all neighbouring buttons: Create react components for four buttons surrounding innerbox or square which is to be colored and pass 'isClicked' prop to Button component i.e. <Button isClicked={}/> and from Button Component pass result of isClicked to a function in App.js whose result of allButtons clicked is passed as a prop to innerBox React component and then if allButtons clicked is true then change color of innerBox from innerBox react component there itself  */}
     </div>
     
