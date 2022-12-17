2513e285879fe5c231f138daf4af21e2b5e067a9
Devansh0309 <samdeva0309@gmail.com>
2022-12-17 11:55:44 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import LeftDrawer from "./NewNavbar/NewNavbar";
 import Contexts from "./Contexts";
 import SquareGrid from "./Square Grid Box/SquareGrid";
 import { Route, Routes } from "react-router-dom";
 import HowToPlay from "./AboutGame/HowToPlay";
 import MyVerticallyCenteredModal from "./NewNavbar/OptionsDialogBox"
 import NewNavbar from "./NewNavbar/NewNavbar";
-import Auth from "./Auth";
-import Saves from './SavedGames/Saves'
+// import Auth from "./Auth";
+// import Saves from './SavedGames/Saves'
 import VerticallyCenteredModal from './StartDialog/index'
 import NewNavbar2 from "./NewNavbar/AboutNavbar";
 
