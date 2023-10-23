788f1cb7c528f5f8f595f49ccd756958148df034
Devansh0309 <samdeva0309@gmail.com>
2023-10-23 10:33:54 +0530

    squareColor not defined bug removed.

--- src/Square Grid Box/GridComponent.jsx
+++ src/Square Grid Box/GridComponent.jsx
@@ -9,19 +9,19 @@ function GridComponent() {
     <div className="main-section">
       <div className="player-scores">
         <div className="player-1-score">
-          {state.player1Name}: {state.player1Score}
+          {state?.player1Name}: {state?.player1Score}
         </div>
         <div className="player-2-score">
-          {state.player2Name}: {state.player2Score}
+          {state?.player2Name}: {state?.player2Score}
         </div>
       </div>
       <div
         className="chance"
         style={{
-          backgroundColor: state.player === "1" ? "#eb5d5d" : "#42c442",
+          backgroundColor: state?.player === "1" ? "#eb5d5d" : "#42c442",
         }}
       >
-        {state.player === "1" ? state.player1Name : state.player2Name} chance
+        {state?.player === "1" ? state?.player1Name : state?.player2Name} chance
       </div>
 
       <div
@@ -29,61 +29,61 @@ function GridComponent() {
         id="grid-box"
         style={{
           //for fixing grid size-->
-          //calc(${state.row+1} * var(--square_height)=(state.row+1)*80, where var(--square_height)=80px
+          //calc(${state?.row+1} * var(--square_height)=(state?.row+1)*80, where var(--square_height)=80px
           height:
-            state.gridWidth >
+            state?.gridWidth >
               document
                 .getElementsByClassName("main-section")[0]
                 ?.getBoundingClientRect()?.width ||
-            state.gridHeight >
+            state?.gridHeight >
               document
                 .getElementsByClassName("main-section")[0]
                 ?.getBoundingClientRect()?.height
               ? "var(--height)"
-              : `calc(${state.row + 1} * var(--square_height))`,
+              : `calc(${state?.row + 1} * var(--square_height))`,
 
           width:
-            state.gridWidth >
+            state?.gridWidth >
             document
               .getElementsByClassName("main-section")[0]
               ?.getBoundingClientRect()?.width
               ? "var(--width)"
-              : `calc(${state.col + 1} * var(--square_width))`,
+              : `calc(${state?.col + 1} * var(--square_width))`,
 
           gridTemplateColumns:
-            state.gridWidth >
+            state?.gridWidth >
             document
               .getElementsByClassName("main-section")[0]
               ?.getBoundingClientRect()?.width
-              ? `repeat(${state.col + 1},calc(var(--width) / ${state.col + 1}))`
-              : `repeat(${state.col + 1},1fr)`,
+              ? `repeat(${state?.col + 1},calc(var(--width) / ${state?.col + 1}))`
+              : `repeat(${state?.col + 1},1fr)`,
 
           gridTemplateRows:
-            state.gridWidth >
+            state?.gridWidth >
               document
                 .getElementsByClassName("main-section")[0]
                 ?.getBoundingClientRect()?.width ||
-            state.gridHeight >
+            state?.gridHeight >
               document
                 .getElementsByClassName("main-section")[0]
                 ?.getBoundingClientRect()?.height
-              ? `repeat(${state.row + 1},calc(var(--width) / ${state.col + 1}))`
-              : `repeat(${state.row + 1},1fr)`,
+              ? `repeat(${state?.row + 1},calc(var(--width) / ${state?.col + 1}))`
+              : `repeat(${state?.row + 1},1fr)`,
 
           //for fixing square(onebox, twobox) in grid size-->
-          // height:`calc(${state.row+1}*var(--height))`,
-          // width:`calc(${state.col+1}*var(--width))`,
-          // gridTemplateColumns:`repeat(${state.col+1},1fr)`,
-          // gridTemplateRows:`repeat(${state.row+1},1fr)`
+          // height:`calc(${state?.row+1}*var(--height))`,
+          // width:`calc(${state?.col+1}*var(--width))`,
+          // gridTemplateColumns:`repeat(${state?.col+1},1fr)`,
+          // gridTemplateRows:`repeat(${state?.row+1},1fr)`
 
-          // transform: `translateX(-calc(var(--width)/${state.col+1}))`
+          // transform: `translateX(-calc(var(--width)/${state?.col+1}))`
           // transform: `translateX(-calc(var(--width) / 2))`
         }}
         onFocus={() => {
           const elem = document.getElementById("#grid-box");
           console.log(
             "calculation width:",
-            (state.col + 1) * 80,
+            (state?.col + 1) * 80,
             " client width ",
             document
               .getElementsByClassName("main-section")[0]
@@ -91,23 +91,23 @@ function GridComponent() {
           );
         }}
       >
-        {state.Box.map((item) =>
-          item % (state.col + 1) === state.col &&
-          item < state.row * state.col + state.row + state.col ? (
+        {state?.Box.map((item) =>
+          item % (state?.col + 1) === state?.col &&
+          item < state?.row * state?.col + state?.row + state?.col ? (
             <div className="twobox" style={{ flexDirection: "column" }} key={item}>
               <div className="dot"></div>
               <button
                 className="sidelastbtn"
                 style={{
-                  backgroundColor: `${state.verticalButtons[item].btncolor}`,
+                  backgroundColor: `${state?.verticalButtons[item]?.btncolor}`,
                   border: `${
-                    state.verticalButtons[item].active
+                    state?.verticalButtons[item]?.active
                       ? "2px solid black"
                       : "none"
                   }`,
                 }}
                 
-                disabled={(state.player1Live && state.playerEnteredRoom && state.playerFixed!= state.player) || state.verticalButtons[item].isClicked}
+                disabled={(state?.player1Live && state?.playerEnteredRoom && state?.playerFixed!= state?.player) || state?.verticalButtons[item]?.isClicked}
                 onClick={() => {
                   setClick(item, "vertical");
                   areAllClicked(item, "vertical");
@@ -115,39 +115,39 @@ function GridComponent() {
                 }}
               ></button>
             </div>
-          ) : item >= state.row * (state.col + 1) ? (
-            item < state.row * state.col + state.row + state.col ? (
+          ) : item >= state?.row * (state?.col + 1) ? (
+            item < state?.row * state?.col + state?.row + state?.col ? (
               <div className="twobox"  key={item}>
                 <div className="dot"></div>
                 <button
                   className="lowerbtn"
                   style={{
                     backgroundColor: `${
-                      state.horizontalButtons[
-                        item - Math.floor(item / (state.col + 1))
-                      ].btncolor
+                      state?.horizontalButtons[
+                        item - Math.floor(item / (state?.col + 1))
+                      ]?.btncolor
                     }`,
                     border: `${
-                      state.horizontalButtons[
-                        item - Math.floor(item / (state.col + 1))
-                      ].active
+                      state?.horizontalButtons[
+                        item - Math.floor(item / (state?.col + 1))
+                      ]?.active
                         ? "2px solid black"
                         : "none"
                     }`,
                   }}
                   disabled={
-                    (state.player1Live && state.playerEnteredRoom && state.playerFixed!= state.player) || 
-                    state.horizontalButtons[
-                      item - Math.floor(item / (state.col + 1))
-                    ].isClicked
+                    (state?.player1Live && state?.playerEnteredRoom && state?.playerFixed!= state?.player) || 
+                    state?.horizontalButtons[
+                      item - Math.floor(item / (state?.col + 1))
+                    ]?.isClicked
                   }
                   onClick={() => {
                     setClick(
-                      item - Math.floor(item / (state.col + 1)),
+                      item - Math.floor(item / (state?.col + 1)),
                       "horizontal"
                     );
                     areAllClicked(
-                      item - Math.floor(item / (state.col + 1)),
+                      item - Math.floor(item / (state?.col + 1)),
                       "horizontal"
                     );
                     audio2.play();
@@ -164,32 +164,32 @@ function GridComponent() {
                 className="upperbtn"
                 style={{
                   backgroundColor: `${
-                    state.horizontalButtons[
-                      item - Math.floor(item / (state.col + 1))
-                    ].btncolor
+                    state?.horizontalButtons[
+                      item - Math.floor(item / (state?.col + 1))
+                    ]?.btncolor
                   }`,
                   border: `${
-                    state.horizontalButtons[
-                      item - Math.floor(item / (state.col + 1))
-                    ].active
+                    state?.horizontalButtons[
+                      item - Math.floor(item / (state?.col + 1))
+                    ]?.active
                       ? "2px solid black"
                       : "none"
                   }`,
                 }}
                 
                 disabled={
-                  (state.player1Live && state.playerEnteredRoom && state.playerFixed!= state.player) || 
-                  state.horizontalButtons[
-                    item - Math.floor(item / (state.col + 1))
-                  ].isClicked
+                  (state?.player1Live && state?.playerEnteredRoom && state?.playerFixed!= state?.player) || 
+                  state?.horizontalButtons[
+                    item - Math.floor(item / (state?.col + 1))
+                  ]?.isClicked
                 }
                 onClick={() => {
                   setClick(
-                    item - Math.floor(item / (state.col + 1)),
+                    item - Math.floor(item / (state?.col + 1)),
                     "horizontal"
                   );
                   areAllClicked(
-                    item - Math.floor(item / (state.col + 1)),
+                    item - Math.floor(item / (state?.col + 1)),
                     "horizontal"
                   );
                   audio2.play();
@@ -198,14 +198,14 @@ function GridComponent() {
               <button
                 className="sidebtn"
                 style={{
-                  backgroundColor: `${state.verticalButtons[item].btncolor}`,
+                  backgroundColor: `${state?.verticalButtons[item]?.btncolor}`,
                   border: `${
-                    state.verticalButtons[item].active
+                    state?.verticalButtons[item]?.active
                       ? "2px solid black"
                       : "none"
                   }`,
                 }}
-                disabled={(state.player1Live && state.playerEnteredRoom && state.playerFixed!= state.player) || state.verticalButtons[item].isClicked}
+                disabled={(state?.player1Live && state?.playerEnteredRoom && state?.playerFixed!= state?.player) || state?.verticalButtons[item]?.isClicked}
                 onClick={() => {
                   setClick(item, "vertical");
                   areAllClicked(item, "vertical");
@@ -217,18 +217,18 @@ function GridComponent() {
                 className="innerBox"
                 style={{
                   backgroundColor:
-                    state.squaresColors[
-                      item - Math.floor(item / (state.col + 1))
-                    ].squarecolor,
-                  border: state.squaresColors[
-                    item - Math.floor(item / (state.col + 1))
-                  ].active
+                    state?.squaresColors[
+                      item - Math.floor(item / (state?.col + 1))
+                    ]?.squarecolor,
+                  border: state?.squaresColors[
+                    item - Math.floor(item / (state?.col + 1))
+                  ]?.active
                     ? "2px solid black"
                     : "none",
                 }}
               >
-                {/* {(state.squaresColors[item-Math.floor(item/(state.col+1))].squarecolor==="#eb5d5d"?state.player1Name:null)||
-                    (state.squaresColors[item-Math.floor(item/(state.col+1))].squarecolor==="#42c442"?state.player2Name:null)} */}
+                {/* {(state?.squaresColors[item-Math.floor(item/(state?.col+1))].squarecolor==="#eb5d5d"?state?.player1Name:null)||
+                    (state?.squaresColors[item-Math.floor(item/(state?.col+1))].squarecolor==="#42c442"?state?.player2Name:null)} */}
               </div>
             </div>
           )
